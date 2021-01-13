c4500e16234015cfb8eafa0ec10f4919fc2b13ac
ffimnsr <ffimnsr@gmail.com>
2021-01-14 00:02:08 +0800

    In the beginning

---
+++ EhloSchema.cs
@@ -0,0 +1,115 @@
+﻿using GraphQL;
+using GraphQL.Resolvers;
+using GraphQL.Types;
+using System;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace GqlNet5Demo
+{
+    public sealed class EhloSchema : Schema
+    {
+        public EhloSchema(IServiceProvider provider) : base(provider)
+        {
+            Query = new EhloQuery();
+            Mutation = new EhloMutation();
+            Subscription = new EhloSubscription();
+        }
+    }
+
+    public sealed class Message
+    {
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+    public sealed class MessageType : ObjectGraphType<Message>
+    {
+        public MessageType()
+        {
+            Field(o => o.Content);
+            Field(o => o.CreatedAt, type: typeof(DateTimeGraphType));
+        }
+    }
+
+    public sealed class EhloQuery : ObjectGraphType
+    {
+        public EhloQuery()
+        {
+            Field<StringGraphType>("greet", description: "A type that returns a simple hello world string", resolve: context => "Hello, World");
+            Field<MessageType>("greetComplex", description: "A type that returns a complex data", resolve: context =>
+            {
+                return new Message
+                {
+                    Content = "Hello, World",
+                    CreatedAt = DateTime.UtcNow,
+                };
+            });
+        }
+    }
+
+    public sealed class MessageInputType : InputObjectGraphType
+    {
+        public MessageInputType()
+        {
+            Field<StringGraphType>("content");
+            Field<DateTimeGraphType>("createdAt");
+        }
+    }
+
+    public sealed class EhloMutation : ObjectGraphType<object>
+    {
+        public EhloMutation()
+        {
+            Field<StringGraphType>("greetMe",
+                    arguments: new QueryArguments(
+                        new QueryArgument<StringGraphType>
+                        {
+                            Name = "name"
+                        }),
+                    resolve: context =>
+                    {
+                        string name = context.GetArgument<string>("name");
+                        string message = $"Hello {name}!";
+                        return message;
+                    });
+
+            Field<MessageType>("echoMessageComplex",
+                    arguments: new QueryArguments(
+                        new QueryArgument<MessageInputType>
+                        {
+                            Name = "message"
+                        }),
+                    resolve: context =>
+                    {
+                        Message message = context.GetArgument<Message>("message");
+                        return message;
+                    });
+        }
+    }
+
+    public sealed class EhloSubscription : ObjectGraphType<object>
+    {
+        public ISubject<string> greetValues = new ReplaySubject<string>(1);
+
+        public EhloSubscription()
+        {
+            AddField(new EventStreamFieldType
+            {
+                Name = "greetCalled",
+                Type = typeof(StringGraphType),
+                Resolver = new FuncFieldResolver<string>(context =>
+                {
+                    var message = context.Source as string;
+                    return message;
+                }),
+                Subscriber = new EventStreamResolver<string>(context =>
+                {
+                    return greetValues.Select(message => message).AsObservable();
+                }),
+            });
+
+            greetValues.OnNext("Hello, World");
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,69 @@
+using GraphQL.Server;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GqlNet5Demo
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
+        {
+            Configuration = configuration;
+            Environment = environment;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddSingleton<EhloSchema>()
+                .AddGraphQL((options, provider) =>
+                {
+                    options.EnableMetrics = Environment.IsDevelopment();
+
+                    var logger = provider.GetRequiredService<ILogger<Startup>>();
+                    options.UnhandledExceptionDelegate = ctx => logger.LogError("{Error} occured", ctx.OriginalException.Message);
+                })
+                .AddSystemTextJson(deserializerSettings => { }, serializerSettings => { })
+                .AddErrorInfoProvider(opt => opt.ExposeExceptionStackTrace = Environment.IsDevelopment())
+                .AddWebSockets()
+                .AddDataLoader()
+                .AddGraphTypes(typeof(EhloSchema));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseWebSockets();
+            app.UseGraphQLWebSockets<EhloSchema>("/graphql");
+
+            app.UseGraphQL<EhloSchema>("/graphql");
+            app.UseGraphQLPlayground();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
